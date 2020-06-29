85ab2a54a8c244a0129ff41b5052a9e7d1a8a29b
lingruiluo <ll3356@columbia.edu>
2020-06-29 13:39:46 -0400

    fix stacked

--- chart_ipynb/bar.py
+++ chart_ipynb/bar.py
@@ -10,16 +10,16 @@ class Bar(chart_setup.Chart_init):
     title = 'Bar Chart'
     chart_type = 'bar'
 
-    def __init__(self, options=None, stacked = False, *pargs, **kwargs):
-        super(Bar, self).__init__(*pargs, **kwargs)
+    def __init__(self, options=None, stacked = False, title = None, *pargs, **kwargs):
+        super(Bar, self).__init__(title = title, *pargs, **kwargs)
         if options is None:
-            self.options = utils.options(
+            options = utils.options(
 					responsive=True,
                     legend=dict(position="top"),
                     title=dict(display=True, text=self.title),
 					tooltips = dict(mode='index', intersect = False),
-					scales = utils.scales(xAxes=dict(stacked = stacked),
-                                          yAxes=dict(stacked = stacked))
+					scales = utils.scales(xAxes=[dict(stacked = stacked)],
+                                          yAxes=[dict(stacked = stacked)])
             )
         self.options = options
         self.reset()
